b0ac88430c467378071332f89d8c3685e7ee303f
Mohamed Shelf <mohamedsaidlearn@gmail.com>
2021-10-16 04:11:52 +0200

    moved background into a separate component implemented avatar container

--- src/App.js
+++ src/App.js
@@ -1,102 +1,15 @@
-import Particles from 'react-tsparticles';
+import React from 'react';
+import AvatarImg from './Components/AvatarImg';
+import Background from './Components/Background';
 
 const App = () => {
-  const particlesInit = (main) => {
-    console.log(main);
-
-    // you can initialize the tsParticles instance (main) here, adding custom shapes or presets
-  };
-
-  const particlesLoaded = (container) => {
-    console.log(container);
-  };
-
   return (
-    <>
-      <Particles
-      id="tsparticles"
-      init={particlesInit}
-      loaded={particlesLoaded}
-      options={{
-        background: {
-          color: {
-            value: "#eee",
-          },
-        },
-        fpsLimit: 100,
-        interactivity: {
-          detectsOn: "canvas",
-          events: {
-            onClick: {
-              enable: true,
-              mode: "push",
-            },
-            onHover: {
-              enable: true,
-              mode: "repulse",
-            },
-            resize: true,
-          },
-          modes: {
-            bubble: {
-              distance: 400,
-              duration: 2,
-              opacity: 0.8,
-              size: 40,
-            },
-            push: {
-              quantity: 4,
-            },
-            repulse: {
-              distance: 200,
-              duration: 0.4,
-            },
-          },
-        },
-        particles: {
-          color: {
-            value: "#333",
-          },
-          links: {
-            color: "#444",
-            distance: 150,
-            enable: true,
-            opacity: 0.5,
-            width: 1,
-          },
-          collisions: {
-            enable: true,
-          },
-          move: {
-            direction: "none",
-            enable: true,
-            outMode: "bounce",
-            random: false,
-            speed: 2,
-            straight: false,
-          },
-          number: {
-            density: {
-              enable: true,
-              value_area: 800,
-            },
-            value: 80,
-          },
-          opacity: {
-            value: 0.5,
-          },
-          shape: {
-            type: "circle",
-          },
-          size: {
-            random: true,
-            value: 5,
-          },
-        },
-        detectRetina: true,
-      }}
-    />
-    </>
+    <React.Fragment>
+      <Background />
+      <div class="portfolio-wrapper">
+        <AvatarImg />
+      </div>
+    </React.Fragment>
   );
 };
 
---
+++ src/Components/AvatarImg.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import { Avatar } from '@material-ui/core';
+
+const AvatarImg = () => {
+  return (
+    <>
+      <Avatar alt="Mohamed Shelf" src="./src/Media/img/avatar.jpg" />
+    </>
+  );
+};
+
+export default AvatarImg;
